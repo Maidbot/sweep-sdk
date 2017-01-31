5c292d15efff6fe244dbdf8d6c70178c24f5e319
dcyoung <dcyoung3@illinois.edu>
2017-01-31 15:07:04 -0800

    Check for any form of MinGW

--- libsweep/config.mk
+++ libsweep/config.mk
@@ -1,11 +1,22 @@
 VERSION_MAJOR = 0
 VERSION_MINOR = 1
 
-UNAME := $(shell uname)
+# read the operating system
+UNAME := $(shell uname -s)
 
+# check if the uname indicates MINGW32 on windows
+ifeq ($(findstring MINGW32,$(UNAME)),MINGW32)
+  #if UNAME contains MINGW32
+  UNAME = MINGW
+else ifeq ($(findstring MSYS,$(UNAME)),MSYS)
+  #if UNAME contains MSYS
+  UNAME = MINGW
+endif
 
+# Set Options + Flags by Operating System
 ifeq ($(UNAME), Linux)
   # For linux platforms
+  target = libsweep.so
   PREFIX ?= /usr
   CFLAGS += -O2 -Wall -Wextra -pedantic -std=c99 -Wnonnull -fvisibility=hidden -fPIC -pthread
   LDFLAGS += -shared -Wl,-soname,libsweep.so.$(VERSION_MAJOR)
@@ -13,13 +24,13 @@ ifeq ($(UNAME), Linux)
 else ifeq ($(UNAME), Darwin)
   # For mac platforms
   $(error macOS build system support missing)
-else ifeq ($(UNAME), MINGW32_NT-6.2)
+else ifeq ($(UNAME), MINGW)
   # For win platforms using MinGW
+  target = libsweep.dll
   PREFIX ?= C:\MinGW
   CC = gcc
   CFLAGS += -O2 -Wall -Wextra -pedantic -std=c99 -Wnonnull -fvisibility=hidden -fPIC -mno-ms-bitfields
   LDFLAGS += -shared -Wl,-soname,libsweep.dll.$(VERSION_MAJOR)
-  #$(error $(UNAME) not yet supported)
 else
   # For all other platforms
   $(error system not supported)
